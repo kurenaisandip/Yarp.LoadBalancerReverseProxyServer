f3092d9e1b36a1285ce8d0f2fdf5cd3837d17392
Sandip <srai68141@gmail.com>
2025-01-01 22:54:32 +0545

    feat: added ip0-rate limiting in the reverse proxy

--- Yarp.LoadBalancerReverseProxyServer/obj/Debug/net8.0/Yarp.LoadBalancerReverseProxyServer.AssemblyInfo.cs
+++ Yarp.LoadBalancerReverseProxyServer/obj/Debug/net8.0/Yarp.LoadBalancerReverseProxyServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yarp.LoadBalancerReverseProxyServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15b50903d81405ad272a938566c9c7b5c437933e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de0485ec4ddf805a51f47f6df3d4722cda42bc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yarp.LoadBalancerReverseProxyServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yarp.LoadBalancerReverseProxyServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
